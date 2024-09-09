caf393f229d24036cf6f4eb7c880530f45b2b0bf
cho <inbrain144@gmail.com>
2024-09-09 15:24:16 +0900

    close #4

--- src/Test2.jsx
+++ src/Test2.jsx
@@ -1,8 +1,20 @@
 // 컴포넌트를 만들 땐 무조건 대문자를 써준다. 컴포넌트는 하나의 태그라고 생각해도 좋음
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
 const Test2 = () =>{
-    return <div>
-        <input name="text"/>
-    </div>
+    const [state, setState] = useState({text:"", log:[]});
+    const onChangeHandler = (e) => {
+        const {text, value} = e.target;
+        return setState({...state, [text]:value, log:[...state.log, value]});
+    }
+    return (
+        <div className="Test2">
+            <input name="text" onChange={onChangeHandler} value={state.name}/>
+            {state.log.map((value)=> <div> {value}</div>)}
+        </div>
+    );
 }
 
 export default Test2;
