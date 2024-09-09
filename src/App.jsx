cb82cb7e6287d715ea9a76ec2440206337a2659a
cho <inbrain144@gmail.com>
2024-09-09 14:46:47 +0900

    close #3

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
-
-// component (jsx)
+//component .jsx
 function App() {
-  const [state, setState] = useState({color : "red", name : ""});
-  const onChangeHandler = (e) =>{
-    setState(e.target.value)
+  const [state, setState] = useState({color: "red", name:"", log: []});
+  const onChangeHandler = (e) => {
+    const {name, value} = e.target;
+    if(name === "name") return setState({...state, [name]:value, log:[...state.log, value]})
+    setState({...state, [name]:value})
   }
-
+  console.log(state)
   return (
     <div className="App">
       <input 
-        type='color'
-        onChange={onChangeHandler} />
-      <div
-        style={{
-          width: '100px', 
-          height: '100px',
-          backgroundColor: state.color}} />
-      <input name = "name" onChange={onChangeHandler} value={state.name}></input>
+      type='color' 
+      name='color'
+      onChange={onChangeHandler} />
+      <div 
+      style={{
+        width:"100px"
+      , height:"100px"
+      , backgroundColor: state.color}}/>
+      <input name="name" onChange={onChangeHandler} value={state.name}/>
+      {state.log.map((value)=> <div> {value}</div>)}
     </div>
   );
 }
---
+++ src/Test2.jsx
@@ -0,0 +1,8 @@
+// 컴포넌트를 만들 땐 무조건 대문자를 써준다. 컴포넌트는 하나의 태그라고 생각해도 좋음
+const Test2 = () =>{
+    return <div>
+        <input name="text"/>
+    </div>
+}
+
+export default Test2;
